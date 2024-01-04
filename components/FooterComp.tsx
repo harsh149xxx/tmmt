3f7f913de307edca1fe795cbc3defb3f3dccb03a
Biswajit Debnath <biswajit.basanti@gmail.com>
2024-01-05 02:42:54 +0530

    Fix: design issue fixed

--- components/FooterComp.tsx
+++ components/FooterComp.tsx
@@ -1,7 +1,9 @@
 "use client"
-import { FaFacebook, FaTwitter,FaInstagram,FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { menuItems } from "./NavbarComp";
 import { usePathname, useRouter } from "next/navigation";
+import Logo from "@/public/assets/logo.jpg"
+import Image from "next/image";
 
 const FooterComp = () => {
     const router = useRouter();
@@ -9,35 +11,51 @@ const FooterComp = () => {
 
     return (
         <div className="bg-white">
-        <footer className="bg-col3 text-black rounded-t-3xl px-8 py-4 flex flex-col md:flex-row gap-4 md:gap-0">
+            <footer className="grid grid-cols-1 gap-4 rounded-t-3xl bg-col3 px-7 py-12 text-black md:grid-cols-3 md:flex-row md:gap-0 md:px-16 lg:grid-cols-4 lg:px-32">
 
-            <div className="left w-full md:w-1/3 flex flex-col lg:pr-28 gap-5">
-                <span className="font-semibold text-2xl">Xremead</span>
-                <span>177D, Picnic Garden Road, West Bengal, Kolkata - 700039</span>
-                <span>Email: <a href="mailto:support@xtremead.in">support@xtremead.in</a> </span>
-            </div>
-            <div className="middle w-full md:w-1/3 flex flex-col md:pl-16 lg:pl-24 gap-2">
-                {menuItems.map((item, index) => (
-                <button className={`w-fit font-semibold hover:text-col5 ` + `${(pathname == `/${item.slug}` || pathname == `${item.slug}`) ? "text-col5" : "text-black"}`} onClick={() => { router.push(item.slug, { scroll: true }) }} >
-                    {item.title}
-                </button>
-                ))}
-            </div>
+                <div className="left flex flex-col gap-5 md:order-1 lg:order-1">
+                    <div className="relative aspect-square w-1/2">
+                        <Image src={Logo} fill alt="company logo" />
+                    </div>
+                    <span className="text-4xl font-semibold">Xremead</span>
+                </div>
 
-            <div className="right w-full md:w-1/3 gap-3 flex-col flex">
-                <h1 className="font-semibold text-xl">Follow Us</h1>
-                <div className="socialIcon flex text-black text-3xl gap-5">
-                    <FaFacebook className="cursor-pointer hover:text-col5"/>
-                    <FaTwitter className="cursor-pointer hover:text-col5"/>
-                    <FaInstagram className="cursor-pointer hover:text-col5"/>
-                    <FaLinkedin className="cursor-pointer hover:text-col5"/>
+                <div className="right flex flex-col gap-3 md:order-3 lg:order-2">
+                    <h1 className="text-2xl font-semibold">Contact Us</h1>
+                    <span>177D, Picnic Garden Road,</span>
+                    <span> West Bengal, Kolkata - 700039</span>
+                    <span>Email: <a href="mailto:support@xtremead.in" className="text-blue-700 underline">support@xtremead.in</a> </span>
                 </div>
-                <div className="bttn flex gap-5 py-5 flex-wrap">
-                    <button className="border-2 rounded-xl font-semibold hover:bg-transparent border-col5 bg-col5 text-black text-base md:text-xl  px-8 py-2">Signup</button>
-                    <button className="border-2 rounded-xl font-semibold border-col5 hover:bg-col5 text-base md:text-xl  px-8 py-2">Login</button>
+
+                <div className="middle flex flex-col gap-2 md:order-2 lg:order-3">
+                    {menuItems.map((item, index) => (
+                        // <button className={`w-fit font-semibold hover:text-col5` + `${(pathname == `/${item.slug}` || pathname == `${item.slug}`) ? "text-col5" : "text-black"}`} onClick={() => { router.push(item.slug, { scroll: true }) }} >
+                        //     {item.title}
+                        // </button>
+                        <button 
+                        className={`w-fit font-semibold hover:text-col5 ${(pathname == `/${item.slug}` || pathname == `${item.slug}`) ? "text-col5" : "text-black"}`} 
+                        // className={`w-fit font-semibold hover:text-col5`} 
+                        onClick={() => { router.push(item.slug, { scroll: true }) }} >
+                            {item.title}
+                        </button>
+                    ))}
                 </div>
-            </div>
-        </footer>
+
+                <div className="right flex flex-col gap-3 md:order-2 lg:order-4">
+                    <h1 className="text-2xl font-semibold">Follow Us</h1>
+                    <div className="socialIcon flex gap-5 text-3xl text-black">
+                        <FaFacebook className="cursor-pointer hover:text-col5" />
+                        <FaTwitter className="cursor-pointer hover:text-col5" />
+                        <FaInstagram className="cursor-pointer hover:text-col5" />
+                        <FaLinkedin className="cursor-pointer hover:text-col5" />
+                    </div>
+                    <div className="bttn flex flex-wrap gap-5 py-5">
+                        <button className="rounded-xl border-2 border-col5 bg-col5 px-8 py-2 text-base font-semibold text-black hover:bg-transparent md:text-xl">Signup</button>
+                        <button className="rounded-xl border-2 border-col5 px-8 py-2 text-base font-semibold hover:bg-col5 md:text-xl">Login</button>
+                    </div>
+                </div>
+
+            </footer>
         </div>
 
     )
