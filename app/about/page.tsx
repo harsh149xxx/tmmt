f879e1b5c20c9a4a7cd1304b97cb12b7e91d3152
Mohit Singh <127690465+singhmohit7057@users.noreply.github.com>
2024-01-12 23:46:31 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "About",
-  description: "Get to know the dedicated and creative minds behind Velocity Click Media Solutions, the trusted platform for performance marketing. Learn how our process, valuable information, and focused solutions can help your brand achieve its marketing goals."
+  description: "Get to know the dedicated and creative minds behind Velocity Click, the trusted platform for performance marketing. Learn how our process, valuable information, and focused solutions can help your brand achieve its marketing goals."
 }
 
 const About = () => {
