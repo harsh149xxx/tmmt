2053377a5c28301d82676c32a634f37e9bdd5e83
Mohit Singh <127690465+singhmohit7057@users.noreply.github.com>
2024-01-12 23:50:50 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const metadata: Metadata = {
   title: "Services",
-  description: "Let Velocity Click" expert team of affiliate marketers help your business reach new heights by leveraging innovative strategies and the power of the digital world. Increase your online visibility, drive traffic to your website, and generate leads with our specialized affiliate marketing services. Contact us today for tailored solutions that will take your business to the next level."
+  description: "Let Velocity Click' expert team of affiliate marketers help your business reach new heights by leveraging innovative strategies and the power of the digital world. Increase your online visibility, drive traffic to your website, and generate leads with our specialized affiliate marketing services. Contact us today for tailored solutions that will take your business to the next level."
 }
 
 
