01e869360cd2478b6a6215c777ce03f031258305
VelocityClick <156493603+VelocityClick@users.noreply.github.com>
2024-01-22 01:14:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,14 +105,18 @@ export default function RootLayout({
           {children}
           <FooterComp />
         </main>
-        {/*         Clarity tracking code for https://velocityclick.in/ */}
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "kpfgqlkq68");
-        </script>
+        {/* Clarity tracking code for https://velocityclick.in/ */}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "kpfgqlkq68");
+            `,
+          }}
+        />
       </body>
     </html>
   )
