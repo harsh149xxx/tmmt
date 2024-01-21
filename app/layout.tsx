772194d47211fb039ea0abbf32d13eaf2749b9ec
VelocityClick <156493603+VelocityClick@users.noreply.github.com>
2024-01-21 23:34:37 +0530

    Update layout.tsx
    added Clarity code from Bing

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
 
+        <!-- Clarity tracking code for https://velocityclick.in/ -->
+          <script> 
+            (function(c,l,a,r,i,t,y){        
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    
+            })(window, document, "clarity", "script", "kpfgqlkq68");
+          </script>
+
         {/* <!-- Standard favicon --> */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
 
