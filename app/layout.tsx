9adee70c2d15a46c98b74267c0c639eb7cb160a7
VelocityClick <156493603+VelocityClick@users.noreply.github.com>
2024-01-22 01:10:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,15 +32,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
 
-        {/*         Clarity tracking code for https://velocityclick.in/ */}
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "kpfgqlkq68");
-        </script>
-
         {/* <!-- Standard favicon --> */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
 
@@ -114,6 +105,14 @@ export default function RootLayout({
           {children}
           <FooterComp />
         </main>
+        {/*         Clarity tracking code for https://velocityclick.in/ */}
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "kpfgqlkq68");
+        </script>
       </body>
     </html>
   )
